4039703e44877e7b1626e836fad40b6cc5efd1cc
Natalie Gillam <ngillam@hotmail.co.uk>
2024-02-07 06:48:31 +0000

    initial commit

---
+++ js/questions.js
@@ -0,0 +1,20 @@
+const questions = [
+
+  // {
+  //   id: 50,
+  //   q: "exampleToCopy",
+  //   choices: ["", "", ""],
+  //   answer: [],
+  //   questionRef: "paper1-2016s-q",
+  // },
+  // example of an image-based question
+  // {
+  //   id: 11,
+  //   q: "How many squares are showing in this pic?",
+  //   choices: ["3", "2", "1"],
+  //   answer: 2,
+  //   img: "img/square.jpg"
+  // questionRef:
+  //   }
+];
+
